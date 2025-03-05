fd00cc9135b37e814999a2846242fc48dd75e568
ajunglalemtur <aiera971@gmail.com>
2025-03-05 14:59:58 +0530

    Updated App structure and added new components

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+      <footer>
+        <p>© 2024 Little Lemon</p>
+        <p>123 Lemon Street, Food Town</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+   
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h1>Welcome to Little Lemon</h1>
+      <p>Enjoy our delicious meals!</p>
+    </main>
+  );
+}
+
+export default Main;
